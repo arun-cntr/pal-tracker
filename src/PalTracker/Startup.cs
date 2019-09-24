6427f51ec16d9d70b331c79d3affd60afc4e05f2
arun-cntr <arun.arya@123@gmail.com>
2019-09-24 17:01:46 +0530

    DataBaseMigrationtoCICD

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 namespace PalTracker
 {
     public class Startup
@@ -32,8 +32,10 @@ namespace PalTracker
                 Configuration.GetValue<string>("MEMORY_LIMIT", "512M"),
                 Configuration.GetValue<string>("CF_INSTANCE_INDEX", "1"),
                 Configuration.GetValue<string>("CF_INSTANCE_ADDR", "127.0.0.1"))); 
-
-            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+           
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
